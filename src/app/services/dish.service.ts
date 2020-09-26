3a88c8dc9f495bdf224a6d88d0bed973202a4035
Ehdaa Said <ehdaasaid99@gmail.com>
2020-09-26 07:03:27 -0700

    RxJs Part 1

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { Observable ,of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +14,17 @@ export class DishService {
 
   constructor() { }
 
-  getDishes(): Promise<Dish[]> {
-    return Promise.resolve(DISHES);
+  getDishes(): Observable<Dish[]> {
+    return of (DISHES).pipe(delay(2000));
   }
 
-  getDish(id: string):Promise<Dish>{
-    return Promise.resolve(DISHES.filter((dish) => (dish.id === id))[0]) ;
+  getDish(id: string): Observable<Dish> {
+    return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
+    
   }
 
-  getFeaturedDish(): Promise<Dish> {
-    return Promise.resolve(DISHES.filter((dish) => dish.featured)[0]);
+  getFeaturedDish(): Observable<Dish> {
+    return  of(DISHES.filter((dish) => dish.featured)[0]).pipe(delay(2000));
+    
   }
 }
--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Leader } from '../shared/leader';
 import { LEADERS } from '../shared/leaders';
+import { Observable , of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +12,19 @@ export class LeaderService {
 
   constructor() { }
 
-  getLeaders():Promise< Leader[]> {
-    return Promise.resolve (LEADERS);
+  getLeaders():Observable<Leader[]> {
+    return of(LEADERS).pipe(delay(2000));
+    
   }
 
-  getLeader(id: string):Promise<Leader>{
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]) ;
+  getLeader(id: string): Observable<Leader> {
+    return of(LEADERS.filter((leader) => (leader.id === id))[0]).pipe(delay(2000));
+    
   }
-  getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => leader.featured)[0]);
+
+  getFeaturedLeader(): Observable<Leader> {
+    return  of(LEADERS.filter((leader) => leader.featured)[0]).pipe(delay(2000));
+    
   }
 
 
