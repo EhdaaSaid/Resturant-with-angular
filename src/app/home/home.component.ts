9d1a6a9eb2a8870a189c55cff967f89145894f59
Ehdaa Said <ehdaasaid99@gmail.com>
2020-11-26 03:50:05 -0800

    Animations Part 2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,18 +6,26 @@ import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
 import { Leader } from '../shared/leader';
 import { LeaderService } from '../services/leader.service';
-
+import { flyInOut , expand} from '../animations/app.animation';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  host: {
+    '[@flyInOut]': 'true',
+    'style': 'display: block;'
+    },
+    animations: [
+      flyInOut(),
+      expand()
+    ]
 })
 
 export class HomeComponent implements OnInit {
 
   dish: Dish;
-  errMess: string;
+  dishErrMess: string;
   promotion: Promotion;
   leader : Leader;
 
@@ -28,7 +36,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.dishservice.getFeaturedDish()
-    .subscribe((dish)=> this.dish =dish);
+    .subscribe((dish)=> this.dish =dish ,
+    errmess => this.dishErrMess = <any>errmess);
 
 
      this.promotionservice.getFeaturedPromotion()
